2a8026343e25a2074560c14f0084a9ad598a9169
eduardopuentescs <eduardo@codestacksystems.com>
2023-09-02 08:43:35 -0600

    Done

--- src/components/Dashboard/index.js
+++ src/components/Dashboard/index.js
@@ -114,8 +114,9 @@ const Dashboard = () => {
             <Contacto/>
         </div>
 
-        <iframe title='Maps Embeeded' src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d119561.90599859795!2d-100.52015897969135!3d20.53451123592089!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x85d345c704ffdf87%3A0x74a0054d0a1fde09!2sSE%C3%91OR%20TORNILLERO!5e0!3m2!1ses-419!2smx!4v1693664902716!5m2!1ses-419!2smx" width="600" height="450" style={{border:0}} allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
-        
+        <div className='maps-container'>
+            <iframe title='Maps Embeeded' src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d119561.90599859795!2d-100.52015897969135!3d20.53451123592089!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x85d345c704ffdf87%3A0x74a0054d0a1fde09!2sSE%C3%91OR%20TORNILLERO!5e0!3m2!1ses-419!2smx!4v1693664902716!5m2!1ses-419!2smx" width="600" height="450" style={{border:0}} allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+        </div>        
         </>
     )
 }
