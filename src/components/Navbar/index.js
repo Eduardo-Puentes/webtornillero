3cf6a278b9b913dce68a1a2e1e1149477eebf762
eduardopuentescs <eduardo@codestacksystems.com>
2023-09-07 12:01:32 -0600

    Done

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -53,9 +53,8 @@ const Navbar = ({itemsCount = 0}) => {
         <div className="top-container">
         <div className="navbar">
                     <div className="navbar-container container">
-                        <label for="burgir">
-                            <input type="checkbox" name="burgir" id="burgir"/>
-                        </label>
+                        <label for="burgir" style={{position:'absolute'}}/>
+                        <input type="checkbox" name="burgir" id="burgir"/>
                         <div className="hamburger-lines">
                             <span className="line line1"></span>
                             <span className="line line2"></span>
